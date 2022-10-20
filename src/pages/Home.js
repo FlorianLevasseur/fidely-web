d1965cdee11c1959f2ce5d3ae630a1d7dbc9ddf4
angeline-cgnt <angelinecoignet@gmail.com>
2022-10-20 12:15:30 +0200

    Affichage tous les stores home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,23 @@
 import { Link } from "react-router-dom";
+import axios from 'axios';
+import { useEffect, useState } from "react";
+import defaultLogo from "../assets/fast-food.png"
 
 const Home = () => {
+  const [stores, setStores] = useState();
+
+  useEffect(() => {
+    axios.get('http://localhost:4000/stores')
+      .then(response => {
+        setStores(response.data)
+      })
+      .catch(err => {
+        console.log(err)
+      });
+  }, []);
+    
   return (
+    <div>
       <nav>
         <ul className="list-unstyled">
           <li className="m-5">
@@ -12,6 +28,15 @@ const Home = () => {
           </li>
         </ul>
       </nav>
+      <div>
+        {stores.map((store) =>
+          <div>
+            <img src={defaultLogo} width="20rem" />
+            <p>{store.name}</p>
+          </div>
+        )}
+      </div>
+    </div>
   )
 };
 
